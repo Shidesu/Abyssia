11b4b0fa2b05019ccb73e09e6c17388479e55773
Kao <anchisialexandre@wanadoo.fr>
2016-02-28 13:19:49 +0100

    Ajout du fichier d'en-tête json. Seul fichier nécessaire pour la manipulation json. Ne contient pas de fonctions pour la création de fichiers. Il faudra donc en faire une à la main pour se simplifier la vie ensuite. Marc tu penses pouvoir le faire ?
    Modification du main pour montrer un exemple d'utilisation du json.

--- client/main.cpp
+++ client/main.cpp
@@ -7,8 +7,10 @@
 #include <iostream>
 #include <string>
 #include <SFML/Graphics.hpp>
+#include "json.h"
 
 using namespace std;
+using namespace nlohmann;
 
 void testFonct(Personnage const& p)
 {
@@ -17,6 +19,50 @@ void testFonct(Personnage const& p)
 
 int main()
 {
+
+	// create an empty structure (null)
+	json j;
+
+	// add a number that is stored as double (note the implicit conversion of j to an object)
+	j["pi"] = 3.141;
+
+	// add a Boolean that is stored as bool
+	j["happy"] = true;
+
+	// add a string that is stored as std::string
+	j["name"] = "Niels";
+
+	// add another null object by passing nullptr
+	j["nothing"] = nullptr;
+
+	// add an object inside the object
+	j["answer"]["everything"] = 42;
+
+	// add an array that is stored as std::vector (using an initializer list)
+	j["list"] = { 1, 0, 2 };
+
+	// add another object (using an initializer list of pairs)
+	j["object"] = { { "currency", "USD" },{ "value", 42.99 } };
+
+	// instead, you could also write (which looks very similar to the JSON above)
+	json j2 = {
+		{ "pi", 3.141 },
+		{ "happy", true },
+		{ "name", "Niels" },
+		{ "nothing", nullptr },
+		{ "answer",{
+			{ "everything", 42 }
+		} },
+		{ "list",{ 1, 0, 2 } },
+		{ "object",{
+			{ "currency", "USD" },
+			{ "value", 42.99 }
+		} }
+	};
+
+	std::string testJSON = j.dump();
+
+	std::cout << testJSON;
 
 	Guerrier Guerrier(100, 50);
 	Franz franzdebog("Franz", "Agressif", 100, 100, 0, 50, 100);
