0e540e9283d334d3142c29ca503ee2feb13f7909
Kao <anchisialexandre@wanadoo.fr>
2016-02-28 18:45:31 +0100

    ajout namespace + test

--- client/rpg/Game.h
+++ client/rpg/Game.h
@@ -1,16 +1,16 @@
-#pragma once
-
-#include "../entity/Personnage.h"
-#include "../graph/PersonnageGraph.h"
-#include "../entity/Guerrier.h"
-#include "../entity/Monster.h"
-#include "../entity/franz.h"
+#include "..\entity\Entity.h"
+#include "..\entity\Franz.h"
+#include "..\entity\Guerrier.h"
+#include "..\entity\Monster.h"
+#include "..\entity\Personnage.h"
+#include "..\json.h"
 #include <cstdlib>
 #include <iostream>
 #include <string>
 #include <SFML/Graphics.hpp>
-#include "../json.h"
 
+
+#pragma once
 class Game
 {
 public:
