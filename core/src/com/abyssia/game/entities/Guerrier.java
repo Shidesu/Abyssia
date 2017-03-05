44373b638c45a13871811730023152f15222c7ff
ALEXANDRE-PC\Alexandre <anchisialexandre@wanadoo.fr>
2017-03-05 12:32:49 +0100

    J'ai trouvé où placer les tiles. Et ensuite quelques vieilles modifs.

--- core/src/com/abyssia/game/entities/Guerrier.java
+++ core/src/com/abyssia/game/entities/Guerrier.java
@@ -13,7 +13,7 @@ public class Guerrier extends Character {
 		super(sprite, level, hp, mana, name, role);
 				
 		
-		System.out.println("Le personnage de classe " + this.roleName + " et de nom " + this.name + " a été créé.");
+		System.out.println("Le personnage de classe " + this.roleName + " et de nom " + this.name + " a Ã©tÃ© crÃ©Ã©.");
 	}
 	
 	public Guerrier(int level, int hp, int mana, String name) {
